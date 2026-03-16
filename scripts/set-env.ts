bc98f72f569b8bd4ab1f8969c287893113e37e79
codewithkarannn <karanvishwakarmapersonal@gmail.com>
2026-03-16 22:01:58 +0530

    changes made for the set-env.ts

--- scripts/set-env.ts
+++ scripts/set-env.ts
@@ -3,6 +3,12 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
+const dir = "./src/environments";
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir, { recursive: true });
+}
+
 const envConfig = `
 export const environment = {
   production: false,
@@ -12,17 +18,10 @@ export const environment = {
     projectId: "${process.env['NG_APP_PROJECT_ID']}",
     storageBucket: "${process.env['NG_APP_STORAGE_BUCKET']}",
     messagingSenderId: "${process.env['NG_APP_MESSAGING_SENDER_ID']}",
-    appId: "${process.env['NG_APP_APP_ID']}",
-    measurementId: "${process.env['NG_APP_MEASUREMENT_ID']}",
+    appId: "${process.env['NG_APP_APP_ID']}"
   }
 };
 `;
 
-fs.writeFileSync(
-  "./src/environments/environment.ts",
-  envConfig
-);
-fs.writeFileSync(
-  "./src/environments/environment.development.ts",
-  envConfig
-);
+fs.writeFileSync("./src/environments/environment.ts", envConfig);
+fs.writeFileSync("./src/environments/environment.development.ts", envConfig);
